b519ca174dc8801d4596b98727e9bc292ae39f06
dharneesh <93208207+dor-emi@users.noreply.github.com>
2026-09-11 20:55:03 +0530

    Remove Emergent-specific frontend health integration

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,10 +1,5 @@
 // craco.config.js
 const path = require("path");
-require("dotenv").config();
-
-const config = {
-  enableHealthCheck: process.env.ENABLE_HEALTH_CHECK === "true",
-};
 
 function makeDevServerV5Compatible(devServerConfig) {
   const {
@@ -43,17 +38,7 @@ function makeDevServerV5Compatible(devServerConfig) {
   return compatibleConfig;
 }
 
-let WebpackHealthPlugin;
-let setupHealthEndpoints;
-let healthPluginInstance;
-
-if (config.enableHealthCheck) {
-  WebpackHealthPlugin = require("./plugins/health-check/webpack-health-plugin");
-  setupHealthEndpoints = require("./plugins/health-check/health-endpoints");
-  healthPluginInstance = new WebpackHealthPlugin();
-}
-
-let webpackConfig = {
+module.exports = {
   eslint: {
     configure: {
       extends: ["plugin:react-hooks/recommended"],
@@ -77,31 +62,8 @@ let webpackConfig = {
           "**/public/**",
         ],
       };
-
-      if (config.enableHealthCheck && healthPluginInstance) {
-        webpackConfig.plugins.push(healthPluginInstance);
-      }
       return webpackConfig;
     },
   },
-};
-
-webpackConfig.devServer = (devServerConfig) => {
-  if (config.enableHealthCheck && setupHealthEndpoints && healthPluginInstance) {
-    const originalSetupMiddlewares = devServerConfig.setupMiddlewares;
-    devServerConfig.setupMiddlewares = (middlewares, devServer) => {
-      if (originalSetupMiddlewares) {
-        middlewares = originalSetupMiddlewares(middlewares, devServer);
-      }
-      setupHealthEndpoints(devServer, healthPluginInstance);
-      return middlewares;
-    };
-  }
-  return devServerConfig;
-};
-
-module.exports = {
-  ...webpackConfig,
-  devServer: (devServerConfig) =>
-    makeDevServerV5Compatible(webpackConfig.devServer(devServerConfig)),
+  devServer: (devServerConfig) => makeDevServerV5Compatible(devServerConfig),
 };
